8cfb5bc8f619c3ec62b94ee387e32dd5db60cc57
ndeamador <ndeamador@gmail.com>
2021-11-11 19:08:39 +1100

    refactor: gameInUserLibrary.updateRating logic moved from resolver to service

--- src/entities/gamesInUserLibrary/resolver.ts
+++ src/entities/gamesInUserLibrary/resolver.ts
@@ -78,44 +78,29 @@ export class GameInUserLibraryResolver {
     @Arg('rating', _type => Int, { nullable: true }) rating: Rating,
   ) {
 
-    console.log(`\nUpdating rating (igdb_game_id: ${igdb_game_id})...\n------------------------------------------------`);
-    // console.log('updating, game rating', igdb_game_id, rating);
+    // console.log(`\nUpdating rating (igdb_game_id: ${igdb_game_id})...\n------------------------------------------------`);
+    // // console.log('updating, game rating', igdb_game_id, rating);
 
-    try {
-      const { userId } = req.session;
+    // try {
+    //   const { userId } = req.session;
 
-      // const gameInLibrary = await GameInUserLibrary.findOne({ igdb_game_id: igdb_game_id, user: { id: userId } });
-      // console.log('UPDATED GAME: ', gameInLibrary);
+    //   const updateResponse = await GameInUserLibrary.update({ igdb_game_id: igdb_game_id, user: { id: userId } }, { rating: rating });
+    //   console.log('Update response: ', updateResponse);
+    //   if (updateResponse.affected === 0) throw Error;
+    //   // if (savedGameInLibrary.affected === 0) throw Error;
+    //   const updatedGame = await GameInUserLibrary.findOne({ igdb_game_id: igdb_game_id, user: { id: userId } });
+    //   console.log('updated game: ', updatedGame || 'fail');
+    //   return updatedGame;
+    //   // return savedGameInLibrary.affected === 1 ? true : false; // return only success/failure
+    // }
+    // catch (err) {
+    //   console.log(`Failed to update rating: ${err}`);
+    //   throw new Error(`Failed to update rating.`);
+    // }
 
-      // // Temporary upsert workaround.
-      // if (!gameInLibrary) {
-
-      //   const foundUser = await User.findOneOrFail({ where: { id: userId } });
-
-      //   const gameInLibrary = new GameInUserLibrary();
-      //   console.log('is this async?', gameInLibrary);
-      //   gameInLibrary.user = foundUser;
-      //   gameInLibrary.igdb_game_id = igdb_game_id;
-      //   gameInLibrary.rating = rating;
-
-      //   const savedGameInLibrary = await gameInLibrary.save();
-      //   console.log('savedgame:', savedGameInLibrary);
-      //   return savedGameInLibrary;
-      // }
-
-      const updateResponse = await GameInUserLibrary.update({ igdb_game_id: igdb_game_id, user: { id: userId } }, { rating: rating });
-      console.log('Update response: ', updateResponse);
-      if (updateResponse.affected === 0) throw Error;
-      // if (savedGameInLibrary.affected === 0) throw Error;
-      const updatedGame = await GameInUserLibrary.findOne({ igdb_game_id: igdb_game_id, user: { id: userId } });
-      console.log('updated game: ', updatedGame || 'fail');
-      return updatedGame;
-      // return savedGameInLibrary.affected === 1 ? true : false; // return only success/failure
-    }
-    catch (err) {
-      console.log(`Failed to update rating: ${err}`);
-      throw new Error(`Failed to update rating.`);
-    }
+    const { userId } = req.session;
+    if (!userId) throw Error;
+    return this.gameInUserLibraryService.updateRating(userId, igdb_game_id, rating)
   }
 
 
--- src/entities/gamesInUserLibrary/service.ts
+++ src/entities/gamesInUserLibrary/service.ts
@@ -30,4 +30,27 @@ export class GameInUserLibraryService {
       throw new Error(`Failed to add game to library: ${err}`);
     }
   }
+
+
+  async updateRating(userId: number, igdb_game_id: number, rating: Rating): Promise<GameInUserLibrary> {
+    console.log(`\nUpdating rating (igdb_game_id: ${igdb_game_id})...\n------------------------------------------------`);
+
+    try {
+
+      const updateResponse = await GameInUserLibrary.update({ igdb_game_id: igdb_game_id, user: { id: userId } }, { rating: rating });
+      console.log('Update response: ', updateResponse);
+
+      if (updateResponse.affected === 0) throw Error;
+      const updatedGame = await GameInUserLibrary.findOne({ igdb_game_id: igdb_game_id, user: { id: userId } });
+      // console.log('updated game: ', updatedGame || 'fail');
+      if (!updatedGame) throw Error;
+      return updatedGame;
+    }
+    catch (err) {
+      console.log(`Failed to update rating: ${err}`);
+      throw new Error(`Failed to update rating.`);
+    }
+  }
+
+
 }
